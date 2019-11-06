1599c27ef5c97b657159d7ca9fa30369240763a4
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2019-11-06 22:28:32 +0700

    [UPDATE] add some attribute

--- src/java/Model/User.java
+++ src/java/Model/User.java
@@ -10,20 +10,18 @@ package Model;
  * @author nar-u
  */
 public class User {
-    
+
     private String username;
     private String password;
+    private String TeacherID;
+    private String StudentID;
+    private String fullname;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getName() {
+    public String getUsername() {
         return username;
     }
 
-    public void setUserName(String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
@@ -34,5 +32,37 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public String getTeacherID() {
+        return TeacherID;
+    }
+
+    public void setTeacherID(String TeacherID) {
+        this.TeacherID = TeacherID;
+    }
+
+    public String getStudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(String StudentID) {
+        this.StudentID = StudentID;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
 }
