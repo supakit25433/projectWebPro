97787f1b62cf160b3de7decd5f6a64a01f820e63
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2019-11-06 22:28:08 +0700

    [UPDATE] change jsp to forward

--- src/java/Servlet/LoginServlet.java
+++ src/java/Servlet/LoginServlet.java
@@ -46,7 +46,7 @@ public class LoginServlet extends HttpServlet {
             }else{
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
-                getServletContext().getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
+                getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
                 return;
             }
         }
