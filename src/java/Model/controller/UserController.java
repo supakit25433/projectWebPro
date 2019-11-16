cdbff093de971894943ec3bb8bdc0256069885e5
Pete <thannakorn.lectival@mail.kmutt.ac.th>
2019-11-16 22:06:52 +0700

    [ADD] add user controller for jpa uses (wait for UPDATE)
    wait for UPDATE

---
+++ src/java/Model/controller/UserController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.controller;
+
+import javax.persistence.EntityManagerFactory;
+import javax.transaction.UserTransaction;
+import jpa.UsersJpaController;
+
+/**
+ *
+ * @author Gamer
+ */
+public class UserController {
+    private final UsersJpaController ujc;
+
+    public UserController(EntityManagerFactory emf,UserTransaction utx) {
+        this.ujc = new UsersJpaController(utx, emf);
+    }
+    
+    
+    
+}
