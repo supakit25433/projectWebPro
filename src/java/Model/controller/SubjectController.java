9ad5e2a04f546a5cc0721130a50285d76a83f4f5
Pete <thannakorn.lectival@mail.kmutt.ac.th>
2019-11-23 22:26:09 +0700

    [UPDATE] method edit subject

--- src/java/Model/controller/SubjectController.java
+++ src/java/Model/controller/SubjectController.java
@@ -7,10 +7,14 @@ package Model.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.transaction.UserTransaction;
 import jpa.QuizesJpaController;
 import jpa.SubjectsJpaController;
+import jpa.exceptions.NonexistentEntityException;
+import jpa.exceptions.RollbackFailureException;
 import jpaClasses.Subjects;
 import jpaClasses.Quizes;
 
@@ -49,4 +53,16 @@ public class SubjectController {
         return quizSubList;
     }
 
+    public void editSubject(Subjects subject) {
+        try {
+            sjc.edit(subject);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(SubjectController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RollbackFailureException ex) {
+            Logger.getLogger(SubjectController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(SubjectController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
 }
