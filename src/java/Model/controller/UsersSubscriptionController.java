3aa3ba70f3cadf666185405d3c3af10aea014c1d
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2019-11-26 00:10:51 +0700

    [CREATE] create UsersSubscriptionController

---
+++ src/java/Model/controller/UsersSubscriptionController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.controller;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.transaction.UserTransaction;
+import jpa.SubjectsJpaController;
+import jpa.UsersJpaController;
+import jpa.UsersSubscriptionJpaController;
+import jpaClasses.Subjects;
+import jpaClasses.UsersSubscription;
+
+/**
+ *
+ * @author nar-u
+ */
+public class UsersSubscriptionController {
+    
+    private final UsersJpaController ujc;
+    private final SubjectsJpaController sjc;
+    private final UsersSubscriptionJpaController usjc;
+
+    public UsersSubscriptionController(EntityManagerFactory emf,UserTransaction utx) {
+        this.ujc = new UsersJpaController(utx, emf);
+        this.sjc = new SubjectsJpaController(utx, emf);
+        this.usjc = new UsersSubscriptionJpaController(utx, emf);
+    }
+    
+    public UsersSubscription findBySubjectID(Subjects subject){
+        List<UsersSubscription> subList = usjc.findUsersSubscriptionEntities();
+        for (int i = 0; i < subList.size(); i++) {
+            if(subList.get(i).getSubjectsSubjectid().toString().equals(subject.toString())){
+                return subList.get(i);
+            }
+        }
+        return null;
+    }
+    
+}
